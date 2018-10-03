10d4ed1df4ff074c91101434c6b1ec60950d6a30
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2018-10-03 20:06:36 +0200

    overflow fixed to hidden

--- js/myJquery.js
+++ js/myJquery.js
@@ -6,6 +6,9 @@ $(document).ready(function () {
     const $navLinks = $navbar.find('a');
     const $window = $(window);
     const $scrollToTopBtn = $('.scroll-to-top-btn');
+    const $familyPic01 = $('.family-pic-01');
+    const $familyPic02 = $('.family-pic-02');
+    const $familyPic03 = $('.family-pic-03');
 
     function navHide() {
         $getNavCollapse.collapse('hide');
@@ -53,6 +56,30 @@ $(document).ready(function () {
     }
     $scrollToTopBtn.on('click', scrollMeToTop);
 
-    $(".candidate-slogan").animate({ right: '0' }, 800);
+    $(".candidate-slogan").animate({ right: '0' }, 1000);
+
+    function familyImage01Visibility() {
+        if ($(this).scrollTop() > 350) {
+            $familyPic01.animate({left: '0'}, 1000);
+        }
+    };
+    function familyImage03Visibility() {
+        if ($(this).scrollTop() > 550) {
+            $familyPic03.animate({ left: '0' }, 2000);
+        }
+    };
+    function familyImage02Visibility() {
+        if ($(this).scrollTop() > 350) {
+            $familyPic02.animate({ right: '0' }, 1250);
+        }
+    };
+    $window.on('scroll', familyImage01Visibility);
+    $window.on('scroll', familyImage03Visibility);
+    $window.on('scroll', familyImage02Visibility);
+
+    function checkWindowPosition() {
+        console.log('ypos', $window.scrollTop());
+    }
+    $window.on('scroll', checkWindowPosition);
 });
 
