c09d08bc95777041b92717159d809615ddd815e5
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2018-10-05 18:54:44 +0200

    Remove function to add px after click on menu, replace overflow hidden from section to div.

--- js/myJquery.js
+++ js/myJquery.js
@@ -19,15 +19,15 @@ $(document).ready(function () {
     $getLink.on('click', navHide);
 
     //subtracts px from id on small screens
-    function mobileWidth() {
-        const windowWidth = $(window).width();
-        if ( windowWidth <= 768){
-            return 100;
-        }
-        else {
-            return 0;
-        }
-    }
+    // function mobileWidth() {
+    //     const windowWidth = $(window).width();
+    //     if ( windowWidth <= 768){
+    //         return 100;
+    //     }
+    //     else {
+    //         return 0;
+    //     }
+    // }
 
     function scrollToSection() {
         const animationTime = 700;
@@ -37,7 +37,7 @@ $(document).ready(function () {
             .addClass('active')
             .siblings()
             .removeClass('active');
-        $page.animate({ scrollTop: $(anchor).offset().top - mobileWidth()}, animationTime);
+        $page.animate({ scrollTop: $(anchor).offset().top }, animationTime);
     }
     $navLinks.on('click', scrollToSection);
 
